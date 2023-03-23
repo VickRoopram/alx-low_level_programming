c1350d7fff0e500373e0be63c7e497576a754602
VickRoopram <vickhal10@gmail.com>
2023-03-23 09:14:54 -0700

    Hope this works

---
+++ 0x0F-function_pointers/1-main.c
@@ -0,0 +1,17 @@
+/**
+ * print_elem - prints an integer
+ * @elem: the integer to print
+ *
+ * Return: Nothing.
+ */
+void print_elem(int elem)
+{
+    printf("%d\n", elem);
+}
+
+/**
+ * print_elem_hex - prints an integer, in hexadecimal
+ * @elem: the integer to print
+ *
+ * Return: Nothing.
+ */
