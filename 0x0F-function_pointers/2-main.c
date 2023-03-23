347eaa632c80ba4a9b77da276aff6299cd9acf2a
VickRoopram <vickhal10@gmail.com>
2023-03-23 09:03:39 -0700

    0x0F

---
+++ 0x0F-function_pointers/2-main.c
@@ -0,0 +1,17 @@
+/**
+ * is_98 - check if a number is equal to 98
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int is_98(int elem)
+{
+    return (98 == elem);
+}
+
+/**
+ * is_strictly_positive - check if a number is greater than 0
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
